f9402365d67ff0ea4d054ac59c1c5ff62517f0a8
李习靖 <22126153+APlxj@users.noreply.github.com>
2018-03-21 18:02:56 +0800

    lottie_android
    AE软件上设计的动画直接用于移动端

--- lottie_android/src/main/java/ap/com/lottie_android/MainActivity.java
+++ lottie_android/src/main/java/ap/com/lottie_android/MainActivity.java
@@ -3,11 +3,19 @@ package ap.com.lottie_android;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.airbnb.lottie.LottieAnimationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    //AE软件上设计的动画直接用于移动端
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final LottieAnimationView animationView = (LottieAnimationView) findViewById(R.id.animation_view);
+        animationView.setAnimation("he.json");
+        animationView.loop(true);
+        animationView.playAnimation();
     }
 }
